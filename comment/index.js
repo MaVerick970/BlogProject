bc4917188807ca34c19d9e7369d86a4f098d6817
MaVerick970 <rehmanmustansar43@gmail.com>
2025-04-06 10:39:23 +0500

    added file structure for a blog, and microservice for post and comments

---
+++ comment/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+
+const comments = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(comments[req.params.id] || []);
+});
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+
+  let commentArray = comments[req.params.id] || [];
+  commentArray.push({ id: commentId, comment: content });
+
+  comments[req.params.id] = commentArray;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4000, () => {
+  console.log("Listening on port 4000");
+});
